971e204e8934ba6004efcf5ba8e23f89e4c99950
Mazionach <mazafesio25@gmail.com>
2023-10-11 06:57:19 -0700

    a

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#/bin/bash
+#include <stdio.h>
+/**
+ * main - Prints and exits
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
