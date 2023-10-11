e32c0bcb8629e3d21ef8b21d41fbe0a1ef40ac96
Mazionach <mazafesio25@gmail.com>
2023-10-11 07:08:07 -0700

    a

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,15 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n
-		Size of an int: %i byte(s)\n
-		Size of a long int: %i byte(s)\n
-		Size of a long long int: %i byte(s)\n
-		Size of a float: %i byte(s)\n",
-		sizeof(char),
-		sizeof(int),
-		sizeof(long),
-		sizeof(long long),
-		sizeof(float));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
