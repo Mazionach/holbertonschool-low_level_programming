2cff3d4eaa8e0a5bfd53caae8a08539464e878da
Mazionach <mazafesio25@gmail.com>
2023-10-11 07:03:32 -0700

    a

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,20 @@
-#/bin/bash
+#include <stdio.h>
+/**
+ * main - Prints sizes
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n
+		Size of an int: %i byte(s)\n
+		Size of a long int: %i byte(s)\n
+		Size of a long long int: %i byte(s)\n
+		Size of a float: %i byte(s)\n",
+		sizeof(char),
+		sizeof(int),
+		sizeof(long),
+		sizeof(long long),
+		sizeof(float));
+	return (0);
+}
