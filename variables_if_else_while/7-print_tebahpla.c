4352a8996ead56cca0a2cf174c670f5a2b6cf5fd
Your Name <you@example.com>
2023-10-11 08:26:49 -0700

    a

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+/**
+ * main - Print tebahpla
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
