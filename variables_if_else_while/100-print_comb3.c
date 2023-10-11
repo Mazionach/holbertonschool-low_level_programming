15bff96020dd49ae0cd21832d79c35a5fde2152d
Your Name <you@example.com>
2023-10-11 09:15:02 -0700

    a

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Print combinations
+ *
+ * Return: Success
+ */
+int main(void)
+{
+
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			if ((i != 0) && (j != 1));
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar(i + '0');
+			putchar(j + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
