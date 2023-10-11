45c8cf519b5b631452e9c67f8f3c7440ae9c3ece
Your Name <you@example.com>
2023-10-11 08:37:37 -0700

    a

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+/**
+ * main - Print combinations
+ *
+ * Return: Success
+ */
+int main(void)
+{
+
+	char i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
