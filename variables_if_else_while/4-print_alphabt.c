ae513721d0ed4b311b01a5ba047cee31d4b2742b
Your Name <you@example.com>
2023-10-11 08:09:36 -0700

    a

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+/**
+ * main - Print alphabet
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = "a"; a <= "<"; a++)
+	{
+		if ((a != "q") && (a != "e"))
+			putchar(a);
+	}
+	putchar("\n");
+	return (0);
+}
