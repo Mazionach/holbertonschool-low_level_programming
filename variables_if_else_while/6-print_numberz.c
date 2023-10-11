184afe8f274e1b0cac7595a978362e183d9315ab
Your Name <you@example.com>
2023-10-11 08:22:27 -0700

    a

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+/**
+ * main - Print numbers 2
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
