1afa5b45cc363f1be175542cc292a8522179c38e
Your Name <you@example.com>
2023-10-11 08:30:31 -0700

    a

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+/**
+ * main - Print hex digits
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	char a, b;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+
+	for (b = 'A'; b != 'F'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
