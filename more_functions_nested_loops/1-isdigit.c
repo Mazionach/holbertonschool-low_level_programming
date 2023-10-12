c8aeddc9e08cf7fb20ae743841f8d62546095688
Your Name <you@example.com>
2023-10-12 09:20:57 -0700

    a

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 /**
  * _isdigit - Check if digit
  *
- * @c: Char to check
+ * @c: Int to check
  *
  * Return: 1 if digit, 0 otherwise
  */
-int _isdigit(char c)
+int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 0 && c <= 9)
 		return (1);
 	return(0)
 }
