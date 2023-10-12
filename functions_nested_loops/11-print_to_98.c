d52d5a4809089839e99447244e1a012118851937
Your Name <you@example.com>
2023-10-12 06:45:04 -0700

    a

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,24 +2,25 @@
 #include <stdio.h>
 /**
  * print_to_98 - Prints from n to 98
+ *
+ * @n: Starting integer
  */
 void print_to_98(int n)
 {
 	while (1)
 	{
-		printf("%i", n);
 		if (n != 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf("%i, ", n);
 			if (n > 98)
 				n--;
 			else if (n < 98)
 				n++;
-		} else
+		}
+		else
 		{
-			break;
+			printf("98\n");
+			return;
 		}
 	}
-	_putchar('\n');
 }
