c69c8dc56d8359649a643aee6af8d848462b7de3
Your Name <you@example.com>
2023-10-12 06:21:23 -0700

    a

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - Prints from n to 98
+ */
+void print_to_98(int n)
+{
+	for (; n < 98; n++)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
