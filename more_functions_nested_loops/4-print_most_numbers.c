4ca4fe06d9debdd14c0d4b769b9f3c2f35a475cf
Your Name <you@example.com>
2023-10-12 09:34:20 -0700

    a

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,13 @@
 #include "main.h"
 /**
- * foo - bar
+ * print_most_numbers - Prints some numbers
  */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+		if (a != '2' && a != '4')
+			_putchar(a);
+	_putchar('\n');
+}
