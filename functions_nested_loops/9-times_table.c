b6e7747e597c7fa75aefb6b630f1cb7b7cea9e2f
Your Name <you@example.com>
2023-10-12 06:10:14 -0700

    a

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,8 +16,9 @@ void times_table(void)
 			if ((j * i / 10) == 0)
 				_putchar(' ');
 			else
-				_putchar(j * i / 10);
-			_putchar(j * i % 10);
+				_putchar(j * i / 10 + '0');
+			_putchar(j * i % 10 + '0');
 		}
+		_putchar('\n');
 	}
 }
