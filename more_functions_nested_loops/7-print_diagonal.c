16e0b51735c62af7f8e8b2a8d90038e44bc1c9a3
Your Name <you@example.com>
2023-10-12 09:51:45 -0700

    a

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,17 @@
 #include "main.h"
 /**
- * foo - bar
+ * print_diagonal - Print a diagonal line
+ *
+ * @n: Line length
  */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('/');
+	}
+}
