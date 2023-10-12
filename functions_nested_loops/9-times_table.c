259d7c288bb2dffc0d44f830b07da9b4f93d5214
Your Name <you@example.com>
2023-10-12 06:08:41 -0700

    a

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,1 +1,23 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+	int i, j;
+	
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		for (j = 1; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			if ((j * i / 10) == 0)
+				_putchar(' ');
+			else
+				_putchar(j * i / 10);
+			_putchar(j * i % 10);
+		}
+	}
+}
