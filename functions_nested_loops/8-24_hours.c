370f7d99a81bc4cccebfcf6abc746184a897cf20
Your Name <you@example.com>
2023-10-12 05:53:38 -0700

    a

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,1 +1,35 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - I'm federal agent Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int h, th, m, tm;
+	int hours;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		m++;
+		if (m > 9)
+		{
+			m = 0;
+			tm++;
+			if (tm > 5)
+			{
+				tm = 0;
+				h++;
+				if (h > 9)
+				{
+					h = 0;
+					th++;
+				}
+			}
+		}
+		_putchar(th + '0');
+		_putchar(h + '0');
+		_putchar(':');
+		_putchar(tm + '0');
+		_putchar(m + '0');
+		_putchar('\n');
+	}
+}
