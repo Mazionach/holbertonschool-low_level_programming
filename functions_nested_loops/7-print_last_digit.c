ff51a0081830a954c8fe8e8897f78f58322e3abd
Your Name <you@example.com>
2023-10-12 05:42:55 -0700

    a

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,13 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - Prints last digit of number
+ *
+ * @n: The number
+ *
+ * Return: The digit
+ */
+int print_last_digit(int n)
+{
+	_putchar(n % 10 + '0');
+	return (n % 10);
+}
