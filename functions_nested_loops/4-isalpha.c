6c97c76d9131aaa41f99e836ad4a72e03f0e0470
Your Name <you@example.com>
2023-10-12 05:27:03 -0700

    a

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,1 +1,14 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - Check if alphabetic
+ *
+ * @c: The char to check
+ *
+ * Return: true if letter, false otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
