cf2cc8a269fda418311b5541c1392337074a1a09
Your Name <you@example.com>
2023-10-13 05:30:19 -0700

    a

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,29 @@
 #include "main.h"
 /**
- * foo - bar
+ * print_triangle - Prints a triangle
+ *
+ * @size: Size of triangle
  */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putcahr('\n');
+		return;
+	}
+
+	for (i = size; i > 0; i--)
+	{
+		for (j = 1; j <= size; j--)
+		{
+			if (j <= size)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
