5b05020b8f6189d6f8e2c0663a0b7700576289fd
Your Name <you@example.com>
2023-10-13 05:49:33 -0700

    a

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Prints largest prime factor
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	int i, largest;
+
+	for (i = 2; i <= sqrt(1231952); i++)
+		if (isprime(i))
+			if (1231852 % i == 0)
+				largest = i;
+	return (0);
+}
+
+/**
+ * isprime - Check if number is prime
+ *
+ * @n: the number
+ *
+ * Returns: 1 if prime
+ */
+int isprime(int n)
+{
+	int i;
+
+	for (i = sqrt(n); i > 1; i--)
+		if (n % i == 0)
+			return (1);
+	return (0);
+}
