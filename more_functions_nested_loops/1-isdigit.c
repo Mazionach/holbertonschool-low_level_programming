fd95f3dff0008bf4bbc9ba5249e5b58a038e6211
Your Name <you@example.com>
2023-10-13 09:28:27 -0700

    a

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,7 @@
 #include "main.h"
-/**
- * _isdigit - Check if digit
- *
- * @c: Int to check
- *
- * Return: 1 if digit, 0 otherwise
- */
 int _isdigit(int c)
 {
-	if ((c >= 48) && (c <= 57))
-		return (1);
-	return (0);
+	if((c >= 48) && (c <= 57))
+		return (1);aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+	return (0);  
 }
