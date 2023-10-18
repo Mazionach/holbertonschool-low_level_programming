2d0916c22ca7b0c94a8bf69d97077c756c932ec9
Mazionach <mazafesio25@gmail.com>
2023-10-18 06:03:17 -0700

    a

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swaps a and b
+ * @a: to b
+ * @b: to a
+ */
+void swap_int(int *a, int *b)
+{
+	int c = a;
+	*a = *b
+	*b = c;
+}
---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - sets n to 98
+ * @n: n to reset
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
