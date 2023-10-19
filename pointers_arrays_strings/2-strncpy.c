5399b06e7f74babac0b75d84782fad4996bed86c
Mazionach <mazafesio25@gmail.com>
2023-10-19 09:48:51 -0700

    Added 'n' parameter

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -3,16 +3,18 @@
  * _strncpy - copies a string to a buffer
  * @dest: final string
  * @src: other string
- * @n: number of chars to concatenate
+ * @n: number of chars to copy
  * Return: pointer to start of buffer
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	while (src[i])
+	while (src[i] && n)
 	{
 		dest[i] = src[i];
 		i++;
+		n--;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
