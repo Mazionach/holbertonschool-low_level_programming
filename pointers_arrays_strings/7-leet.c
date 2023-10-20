b378ca1b39bbe4a648a14fd587739d044bbbe59e
Mazionach <mazafesio25@gmail.com>
2023-10-20 08:50:32 -0700

    Fixed trailing whitespace

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@ char *leet(char *s)
 	int j, i = 0;
 	char chr[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char num[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-	
+
 	while (s[i])
 	{
 		for (j = 0; j < 10; j++)
