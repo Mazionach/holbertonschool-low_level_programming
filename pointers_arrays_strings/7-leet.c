5e8dcf3549bcd36ceedf074bce1697ef15159d01
Mazionach <mazafesio25@gmail.com>
2023-10-20 08:47:34 -0700

    Added leet encoding function

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * leet - encodes a string in 1337
+ * @s: string to encode
+ * Return: encoded string
+ */
+char *leet(char *s)
+{
+	int j, i = 0;
+	char chr[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char num[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	
+	while (s[i])
+	{
+		for (j = 0; j < 10; j++)
+			if (s[i] == chr[j])
+				s[i] = num[j];
+		i++;
+	}
+	return (s);
+}
