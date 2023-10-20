b4a8af4e45914a8e27713c7a5cd3073d1ddea7bf
Mazionach <mazafesio25@gmail.com>
2023-10-20 05:26:21 -0700

    Strncpy hotfix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,18 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+
 	while (src[i] && n)
 	{
 		dest[i] = src[i];
 		i++;
 		n--;
 	}
-	dest[i] = '\0';
+	while (dest[i] && n)
+	{
+		dest[i] = '\0';
+		i++;
+		n--;
+	}
 	return (dest);
 }
