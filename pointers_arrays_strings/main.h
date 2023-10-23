1b37704cc6d378991ca7d085861b83b63705e5a9
Mazionach <mazafesio25@gmail.com>
2023-10-23 05:42:08 -0700

    Added memset function and extended main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,3 +23,14 @@ void reverse_array(int *, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+
+
+char *_memset(char *, char, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *, int);
+
