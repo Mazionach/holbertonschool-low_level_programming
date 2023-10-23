7e0a4755ec07f37ed78ec2768a049befc6d413cf
Mazionach <mazafesio25@gmail.com>
2023-10-23 09:28:39 -0700

    Function to print a string

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
 	else
 	{
-		_putchar(*s);
-		_puts_recursion(++s);
+		_putchar('\n');
 	}
 }
