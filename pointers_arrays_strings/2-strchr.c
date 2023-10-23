2cbd5d382d73b5b4c39474a53c5e8db75820d25b
Mazionach <mazafesio25@gmail.com>
2023-10-23 06:04:58 -0700

    Added character search function

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strchr - searches for a character in a string
+ * @s: the string
+ * @c: the character to search
+ * Return: ponter to the first occurrence, or null
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+		if (*s == c)
+			return (s);
+		else
+			s++;
+	return (0);
+}
