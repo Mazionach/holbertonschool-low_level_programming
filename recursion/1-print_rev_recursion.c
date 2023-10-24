a805366430d6639dfcaee22991f615c91787cd9f
Mazionach <mazafesio25@gmail.com>
2023-10-24 05:18:15 -0700

    a

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,22 +1,13 @@
 #include "main.h"
-void re_rev(char *s);
 /**
  * _print_rev_recursion - Prints string in reverse with recursion
  * @s: the string
  */
 void _print_rev_recursion(char *s)
 {
-	re_rev(s);
-}
-/**
- * re_rev - The recursion of _print_rev_recursion
- * @s: the string
- */
-void re_rev(char *s)
-{
 	if (*s)
 	{
-		re_rev(++s);
+		_print_rev_recursion(++s);
 		_putchar(*s);
 	}
 }
