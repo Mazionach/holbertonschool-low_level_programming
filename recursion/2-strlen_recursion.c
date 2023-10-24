03c3c52a15bf0202be256abb29df676f62e4fcd5
Mazionach <mazafesio25@gmail.com>
2023-10-24 05:27:35 -0700

    Functin to calculate string length

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Calculates length of string
+ * @s: the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (_strlen_recursion(++s) + 1);
+	return (0);
+}
