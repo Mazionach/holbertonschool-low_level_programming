b3aa70cf3519d3d64dbbec09787de8ad3bb500b1
Mazionach <mazafesio25@gmail.com>
2023-10-26 07:28:14 -0700

    Program to sum numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - Sums numbers
+ * @argc: argument count
+ * @argv: argument list
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	int j, sum = 0;
+	unsigned int i;
+
+	for (j = 0; j < argc; j++)
+	{
+		for (i = 0; i < strlen(argv[j]); i++)
+			if (!isdigit(argv[j][i]))
+				{
+				printf("Error\n");
+				return (1);
+				}
+		sum = sum + atoi(argv[j]);
+	}
+	return (0);
+}
