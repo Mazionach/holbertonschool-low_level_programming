9dd21a2d3169b2cb177efc089f7d595d4a8b9cb1
Mazionach <mazafesio25@gmail.com>
2023-10-26 06:23:18 -0700

    Program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print own name
+ * @argc: command line argument count
+ * @argv: command line argument list
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
