a6b127281e6b940dbf5c3a929ce5e938f59e7a40
Mazionach <mazafesio25@gmail.com>
2023-11-01 06:17:34 -0700

    Function to create and fill array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * create_array - create and initialize array
+ * @c: the byte to initialize to
+ * @size: the size of the memory
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *r = malloc(sizeof(char) * size);
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		r[i] = c;
+	return (r);
+}
