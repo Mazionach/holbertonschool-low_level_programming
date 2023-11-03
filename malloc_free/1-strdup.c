6ed36b4c8e41daeef4158cc81bf22c09358598c2
Mazionach <mazafesio25@gmail.com>
2023-11-03 05:42:47 -0700

    Function to copy string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - duplicate string
+ * @str: the string
+ *
+ * Return: the copy of string
+ */
+char *_strdup(char *str)
+{
+	char *r;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+	*r = malloc(strlen(str) + 1);
+	for (i = 0; i <= strlen(str); i++)
+		r[i] = str[i];
+	return (r);
+}
