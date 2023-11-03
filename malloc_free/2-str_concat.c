c2755ec406c6f1da334100b1d0ae362601e22811
Mazionach <mazafesio25@gmail.com>
2023-11-03 06:10:23 -0700

    a

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,15 +10,21 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *r;
-	unsigned int i, j;
+	unsigned int i, j, l = 1;
 
+	if (s1)
+		l += strlen(s1);
+	if (s2)
+		l += strlen(s2);
 	r = malloc(strlen(s1) + strlen(s2) + 1);
 	if (!r)
 		return (NULL);
-	for (i = 0; i < strlen(s1); i++)
-		r[i] = s1[i];
-	for (j = 0; j < strlen(s2); j++)
-		r[i + j] = s2[j];
+	if (s1)
+		for (i = 0; i < strlen(s1); i++)
+			r[i] = s1[i];
+	if (s2)
+		for (j = 0; j < strlen(s2); j++)
+			r[i + j] = s2[j];
 	r[i + j] = '\0';
 	return (r);
 }
