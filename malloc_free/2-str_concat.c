b7d279e4c86dd1a601bd2d3c1e0b63e702f22699
Mazionach <mazafesio25@gmail.com>
2023-11-03 05:55:37 -0700

    Function to concatenate strings

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - concatenate strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: the resulting string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *r;
+	unsigned int i, j;
+
+	if (!s1 && !s2)
+		return (NULL);
+	r = malloc(strlen(s1) + strlen(s2) + 1);
+	if (!r)
+		return (NULL);
+	for (i = 0; i <= strlen(s1); i++)
+		r[i] = s1[i];
+	for (j = 0; j <= strlen(s2); j++)
+		r[i + j] = s2[j];
+	return (r);
+}
