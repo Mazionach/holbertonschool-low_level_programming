c1b5b2afd0b19a9672ee42d9d7e7e698b4ae6585
Mazionach <mazafesio25@gmail.com>
2023-11-03 05:58:39 -0700

    Concatenate hotfix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,14 +12,13 @@ char *str_concat(char *s1, char *s2)
 	char *r;
 	unsigned int i, j;
 
-	if (!s1 && !s2)
-		return (NULL);
 	r = malloc(strlen(s1) + strlen(s2) + 1);
 	if (!r)
 		return (NULL);
-	for (i = 0; i <= strlen(s1); i++)
+	for (i = 0; i < strlen(s1); i++)
 		r[i] = s1[i];
-	for (j = 0; j <= strlen(s2); j++)
+	for (j = 0; j < strlen(s2); j++)
 		r[i + j] = s2[j];
+	r[i + j] = '\0';
 	return (r);
 }
