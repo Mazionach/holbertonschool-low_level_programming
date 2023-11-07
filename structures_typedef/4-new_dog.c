d78a86bdef7ecbcd425277d8c764d1603df40a6d
Mazionach <mazafesio25@gmail.com>
2023-11-07 05:43:23 -0800

    Added function to create dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - allocates dog
+ * @name: the name
+ * @age: the age
+ * @owner: the owner's name
+ *
+ * Return: created dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeof(struct dog));
+
+	if (!d)
+		return (NULL);
+
+	d->name = strdup(name);
+	d->age = age;
+	d->owner = strdup(owner);
+
+	return (d);
+}
