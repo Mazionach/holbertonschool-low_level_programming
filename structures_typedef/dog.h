e58156e049d82c1bd3ae2ecd5d5609e86cd56edb
Mazionach <mazafesio25@gmail.com>
2023-11-07 05:33:03 -0800

    Fixed prototype and typedef for print_dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,6 +16,8 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *, char *, float, char *);
+typedef struct dog dog_t;
 
+void init_dog(struct dog *, char *, float, char *);
+void print_dog(struct dog *);
 #endif
