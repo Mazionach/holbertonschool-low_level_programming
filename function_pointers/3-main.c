eed2c916f4260f3dfd1b302ea1d3e05af30a085a
Mazionach <mazafesio25@gmail.com>
2023-11-11 09:39:44 -0800

    Changed includes order

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-#include "3-get_op_func.c"
 #include "3-op_functions.c"
+#include "3-get_op_func.c"
 /**
  * main - calculator program
  * @argc: argument count
