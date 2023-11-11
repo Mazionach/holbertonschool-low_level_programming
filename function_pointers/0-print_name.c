988c8d66485a4449e0468d1ef6c769430bec201a
Mazionach <mazafesio25@gmail.com>
2023-11-11 06:30:22 -0800

    Added print name function

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * print_name - prints name using another function
+ * @name: the name
+ * @f: the actual name printing function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
