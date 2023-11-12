2558a24cd0ebd9c38c0faee0d16b41d836bc31b4
Mazionach <mazafesio25@gmail.com>
2023-11-12 13:58:50 -0800

    Added print variable strings function

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <stdarg.h>
 /**
- * a - a
- * @a:
+ * print_strings - prints strings
+ * @separator: separator string
+ * @n: ammout of parameters
  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+
+	va_list l;
+
+	va_start(l, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(l, int);
+		if (s)
+			printf("%s", s);
+		else
+			printf("(nil)");
+		if ((i != n - 1) && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(l);
+}
