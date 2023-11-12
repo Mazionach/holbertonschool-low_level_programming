8722e9c58e333975989f6ec6e621105d549afe3c
Mazionach <mazafesio25@gmail.com>
2023-11-12 12:47:01 -0800

    Added sum of varible numbers function

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - sums numbers
+ * @n: ammout of parameters
+ * Return: the result
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int r = 0;
+
+	va_list l;
+
+	va_start(l, n);
+
+	for (i = 1; i < n; i++)
+	{
+		r += va_arg(l, int);
+	}
+	va_end(l);
+	return (r);
+}
