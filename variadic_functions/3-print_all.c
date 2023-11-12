371d6bc148eadd829d92920f9cfa24b477610cc9
Mazionach <mazafesio25@gmail.com>
2023-11-12 14:14:32 -0800

    Added print variable stuff function

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - prints stuff
+ * @format: the format of the stuff
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	char *s;
+	va_list l;
+
+	va_start(l, format);
+
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(l, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(l, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(l, double));
+				break;
+			case 's':
+				s = va_arg(l, char *);
+				if (s)
+					printf("%s", s);
+				else
+					printf("(nil)");
+				break;
+		}
+		i++;
+		if (format[i])
+			printf(", ");
+	}
+	printf("\n");
+	va_end(l);
+}
