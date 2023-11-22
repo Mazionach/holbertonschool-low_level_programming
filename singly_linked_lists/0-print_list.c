27487db9c10c14ea326ecb8b6b0d0643a90c4613
Mazionach <mazafesio25@gmail.com>
2023-11-22 06:41:20 -0800

    Added function to print lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - prints elements of a list
+ * @h: the list's first element
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int a = 0;
+
+	while (h)
+	{
+		if (h->str)
+			printf("[%i] %s", h->len, h->str);
+		else
+			printf("[0] (nul)");
+		a++;
+		h = h->next;
+	}
+	return (a);
+}
