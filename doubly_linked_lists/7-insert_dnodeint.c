5cea1d05fd66d757cb85dc8a9638ea89e5c9dabf
Mazionach <mazafesio25@gmail.com>
2023-11-27 10:33:12 -0800

    Added function to insert element at index of list

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts element at index of list
+ * @h: pointer to list
+ * @idx: index
+ * @n: element
+ * Return: address of new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp, *cur = *h;
+
+	if (!(*h || idx))
+		return (add_dnodeint(h, n));
+
+	while (idx && cur)
+	{
+		cur = cur->next;
+		idx--;
+	}
+	if (!idx)
+	{
+		return (add_dnodeint(&(cur->prev->next), n));
+	}
+	return (NULL);
+}
