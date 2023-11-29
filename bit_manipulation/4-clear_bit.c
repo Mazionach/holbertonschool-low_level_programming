2aadf34378b62f48ee24248ded0e13f45152007c
Mazionach <mazafesio25@gmail.com>
2023-11-29 10:59:36 -0800

    Added function to clear bit

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,8 +1,21 @@
 #include "main.h"
 
 /**
- * a - a
- * @b:
- * Return:
+ * clear_bit - clear given bit of int
+ * @n: the int
+ * @index: the bit
+ * Return: success
  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int b = 1;
 
+	if (index > 31)
+		return (-1);
+
+	b = b << index;
+
+	*n = *n & (~b);
+
+	return (1);
+}
