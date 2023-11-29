f9be75879ce248ade35476f81ac36f6cdceb603c
Mazionach <mazafesio25@gmail.com>
2023-11-29 10:38:24 -0800

    Added function to print binary representation

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,8 +1,26 @@
 #include "main.h"
 
 /**
- * a - a
- * @b:
- * Return:
+ * print_binary - prints binary representation
+ * @n: the int
  */
+void print_binary(unsigned long int n)
+{
+	unsigned long int b = 0x8000000000000000;
 
+	while (!(n & b) && b)
+		b = b >> 1;
+	if (!b)
+	{
+		printf("0");
+		return;
+	}
+	while (b)
+	{
+		if (b & n)
+			printf("1");
+		else
+			printf("0");
+		b = b >> 1;
+	}
+}
