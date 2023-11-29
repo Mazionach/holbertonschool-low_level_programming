a36ef5ef1546e376dfc2fe8d6493e59aaf5330ed
Mazionach <mazafesio25@gmail.com>
2023-11-29 11:07:34 -0800

    Added function to count different bits

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
 /**
- * a - a
- * @b:
- * Return:
+ * flip_bits - returns number of different bits
+ * @n: int 1
+ * @m; int 2
+ * Return: the difference
  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int b = 1;
+	unsigned int r = 0;
 
+	for (; b; b = b << 1)
+		if ((n ^ m) & b)
+			r++;
+	return (r);
+}
