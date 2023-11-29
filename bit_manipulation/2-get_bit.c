59c18e460e0729ec34395dc6e8c3bdeda3d056b6
Mazionach <mazafesio25@gmail.com>
2023-11-29 10:47:23 -0800

    Added function to get bit of int

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,8 +1,22 @@
 #include "main.h"
 
 /**
- * a - a
- * @b:
- * Return:
+ * get_bit - gets value of given bit of int
+ * @n: the int
+ * @index: the bit
+ * Return: the value, or -1 if error
  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int b = 1;
 
+	if (index > 31)
+		return (-1);
+
+	b = b << index;
+
+	if (n & b)
+		return (1);
+	else
+		return (0);
+}
