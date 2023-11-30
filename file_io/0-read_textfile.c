abbeef54aff5f2d1f2b18b16ddbfcb2f617730fa
Mazionach <mazafesio25@gmail.com>
2023-11-30 08:48:58 -0800

    Added function to copy file to stdout

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads and prints a file
+ * @filename: the file name
+ * @letters: max letters to print
+ * Return: number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	unsigned int r, f, tmp;
+	char *buf;
+
+	if (!filename)
+		return (0);
+	f = open(filename, O_RDONLY);
+	buf = malloc(letters);
+
+	if (!buf)
+		return (0);
+	r = read(f, buf, letters);
+
+	tmp = write(1, buf, r);
+	free(buf);
+	close(f);
+
+	if (tmp != r)
+		return (0);
+	return (r);
+}
