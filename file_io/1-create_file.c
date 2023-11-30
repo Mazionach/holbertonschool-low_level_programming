69d232d70aa1a493035518e4656681a89ad41168
Mazionach <mazafesio25@gmail.com>
2023-11-30 10:33:01 -0800

    Added function to create files

---
+++ file_io/1-create_file.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the file name
+ * @text_content: content of file
+ * Return: success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+
+	if (!filename)
+		return (-1);
+	f = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0x180);
+
+	if (text_content)
+		if (write(f, text_content, strlen(text_content)) == -1)
+			return (-1);
+	return (1);
+}
