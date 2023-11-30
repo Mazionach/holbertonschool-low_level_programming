853bf564fab8f4ebf3885d0b70742da50268b2f7
Mazionach <mazafesio25@gmail.com>
2023-11-30 10:37:20 -0800

    Fixed betty style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	f = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0x180);
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0x180);
 
 	if (text_content)
 		if (write(f, text_content, strlen(text_content)) == -1)
