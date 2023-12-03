40c3d8e9208ff80913a6165cc8c422f1a6a8b23e
Mazionach <mazafesio25@gmail.com>
2023-12-03 14:17:36 -0800

    Fixed collision problem

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -22,8 +22,6 @@ void hash_table_print(const hash_table_t *ht)
 				printf(", ");
 			primero = 0;
 			printf("'%s': '%s'", cur->key, cur->value);
-			if (cur->next)
-				printf(", ");
 		}
 	}
 	printf("}\n");
