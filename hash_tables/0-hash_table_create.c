be302e2113dbcf64f8a082eeb5e4752a534a2a46
Mazionach <mazafesio25@gmail.com>
2023-12-03 09:06:05 -0800

    Fixed more betty style

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 	hash_node_t **array = malloc(sizeof(*array) * size);
-	long unsigned int i;
+	unsigned long int i;
 
 	if (!table || !array)
 	{
