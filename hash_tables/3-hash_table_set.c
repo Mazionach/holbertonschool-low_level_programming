b0435c42b820f7a8e5f11fa48406f6c4713f2e4e
Mazionach <mazafesio25@gmail.com>
2023-12-03 12:45:20 -0800

    Fixed betty style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,13 +14,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!key || !ht)
 		return (0);
-
 	i = key_index((unsigned char *)key, ht->size);
-
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
 		return (0);
-
 	new->key = strdup(key);
 	new->value = strdup(value);
 	if (ht->array[i])
