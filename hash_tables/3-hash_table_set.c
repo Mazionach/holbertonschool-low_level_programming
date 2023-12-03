a65991770934397f7076f948577e6ddba41bb207
Mazionach <mazafesio25@gmail.com>
2023-12-03 11:31:05 -0800

    Added function to add node to hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,7 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * a - a
- * @a: a
- * Return: a
+ * hash_table_set - adds element to hash table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value
+ * Return: success
  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	int i;
+
+	if (!key || !ht)
+		return (0);
+
+	i = hash_djb2(key) % ht->size;
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	if (ht->array[i])
+		new->next = ht->array[i];
+	else
+		new->next = NULL;
+
+	ht->array[i] = new;
+	return (1);
+}
