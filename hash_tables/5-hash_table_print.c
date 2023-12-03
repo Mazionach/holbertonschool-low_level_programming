329af245e008849185e026b812583e60daff741c
Mazionach <mazafesio25@gmail.com>
2023-12-03 14:12:48 -0800

    Added function to print hash table

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,7 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * a - a
- * @a: a
- * Return: a
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
  */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *cur;
+	unsigned int i;
+	int primero = 1;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		for (cur = ht->array[i]; cur; cur = cur->next)
+		{
+			if (!primero)
+				printf(", ");
+			primero = 0;
+			printf("'%s': '%s'", cur->key, cur->value);
+			if (cur->next)
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}
