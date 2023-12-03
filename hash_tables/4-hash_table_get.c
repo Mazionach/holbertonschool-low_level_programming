d5480f2cbedc42ed7142b5387742f6365086025a
Mazionach <mazafesio25@gmail.com>
2023-12-03 13:01:24 -0800

    Added function to get value from key of hash table

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,7 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * a - a
- * @a: a
- * Return: a
+ * hash_table_get - gets value from hash table
+ * @ht: the hash table
+ * @key: the key
+ * Return: the value
  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *cur;
+	int i;
+
+	if (!ht)
+		return (NULL);
+
+	i = key_index((unsigned char *)key, ht->size);
+
+	if (!ht->array[i])
+		return (NULL);
+
+	for (cur = ht->array[i]; cur; cur = cur->next)
+	{
+		if (strcmp(cur->key, key) == 0)
+			break;
+	}
+	if (cur)
+		return (cur->value);
+	return (NULL);
+}
