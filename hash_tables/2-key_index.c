b22a0172a2ced8bb81bd580aac975cba32a86119
Mazionach <mazafesio25@gmail.com>
2023-12-03 09:17:20 -0800

    Added function to get index from key

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,7 +1,14 @@
 #include "hash_tables.h"
 
 /**
- * a - a
- * @a: a
- * Return: a
+ * key_index - gets the index of a certain key
+ * @key: the key
+ * @size: table size
+ * Return: the index
  */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (!key)
+		return (0);
+	return (hash_djb2(key) % size);
+}
