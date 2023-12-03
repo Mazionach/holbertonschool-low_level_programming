bee88b6510646b40e3a310f6222be6426285bed3
Mazionach <mazafesio25@gmail.com>
2023-12-03 12:10:06 -0800

    Fixed signedness problems

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!key || !ht)
 		return (0);
 
-	i = hash_djb2(key) % ht->size;
+	i = key_index((unsigned char *)key, ht->size);
 
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
