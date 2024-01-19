e93787626220bf90c7551fcea88c8c6954b4bcb1
Mazionach <mazafesio25@gmail.com>
2024-01-19 11:38:20 -0800

    Search algorithms initial commit

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+
+
+#ifndef SEARCH_ALGORITHMS
+#define SEARCH_ALGORITHMS
+
+#include <stdio.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+
+int binary_search(int *array, size_t size, int value);
+
+#endif
