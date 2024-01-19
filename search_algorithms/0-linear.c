676d7202c174417bd3dd604ac95cdee23ac37033
Mazionach <mazafesio25@gmail.com>
2024-01-19 11:50:16 -0800

    Added linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,1 +1,22 @@
-$#include "search_algos.h"
+#include "search_algos.h"
+
+/**
+ * linear_search - searches array linearly
+ * @array: the array
+ * @size: array size
+ * @value: objective
+ *
+ * Return: index of value, or NULL
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
